45dcf2cde26b66aff9080ad2a7d4ea5e1aec3a5a
wrapperup <wrapperup4@gmail.com>
2025-01-31 09:41:29 -0800

    add p4v + winkhawk, re-organize

--- install_programs.ps1
+++ install_programs.ps1
@@ -1,25 +1,32 @@
+#Requires -RunAsAdministrator
+
 winget install Mozilla.Firefox
 
-winget install Discord.Discord
-winget install Zulip.Zulip
+winget install wez.wezterm -h
+winget install Microsoft.PowerShell -h
+winget install Git.Git -h --custom '/Components="gitlfs" /EditorOptions=VIM /DefaultBranchName=main /PathOption=Cmd /SSHOption=ExternalOpenSSH /CurlOption=WinSSL /CRLFOption=CRLFAlways /BashTerminalOption=ConHost /UseCredentialManager=Enabled /PerformanceTweaksFSCache=Enabled /EnableSymlinks=Disabled /EnablePseudoConsoleSupport=Disabled /EnableFSMonitor=Enabled'
+winget install Neovim.Neovim -h
 
-winget install wez.wezterm
-winget install Microsoft.PowerShell
-winget install Git.Git --custom '/Components="gitlfs" /EditorOptions=VIM /DefaultBranchName=main /PathOption=Cmd /SSHOption=ExternalOpenSSH /CurlOption=WinSSL /CRLFOption=CRLFAlways /BashTerminalOption=ConHost /UseCredentialManager=Enabled /PerformanceTweaksFSCache=Enabled /EnableSymlinks=Disabled /EnablePseudoConsoleSupport=Disabled /EnableFSMonitor=Enabled'
-winget install Neovim.Neovim
+winget install GlazeWM -h
+winget install AltSnap.AltSnap -h
+winget install RamenSoftware.Windhawk -h
 
-winget install GlazeWM
+winget install Discord.Discord -h
+winget install Zulip.Zulip -h
 
-winget install Docker.DockerDesktop
+winget install Docker.DockerDesktop -h
 
-winget install --id Microsoft.VisualStudio.2022.Community --override "--passive --config ./my.vsconfig"
+winget install Microsoft.VisualStudio.2022.Community -h --override "--passive --config ./my.vsconfig"
 
-winget install JetBrains.Toolbox
-winget install JetBrains.Rider
+winget install JetBrains.Toolbox -h
+winget install JetBrains.Rider -h
 
-winget install Rustlang.Rustup
+winget install Rustlang.Rustup -h
 cargo install ripgrep
 
-winget install OpenJS.NodeJS
-winget install pnpm.pnpm
-winget install CoreyButler.NVMforWindows
+winget install OpenJS.NodeJS -h
+winget install pnpm.pnpm -h
+winget install CoreyButler.NVMforWindows -h
+
+winget install Perforce.P4V -h
+winget install WireGuard.WireGuard -h
